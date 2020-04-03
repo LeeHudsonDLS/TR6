b7dccf183aadb93a1b714e7bf8d14c02c2b9f429
lee <lee.hudson1384@yahoo.co.uk>
2020-04-03 09:52:44 +0100

    Got basic XML commands working

--- simulator/Pcs8000Controller.py
+++ simulator/Pcs8000Controller.py
@@ -1,70 +1,39 @@
 from State import State
+import logging
 import xmltodict
 import xml.etree.ElementTree as ET
 
 class Pcs8000Controller:
     def __init__(self,slaveNo):
+        logging.info(f'Pcs8000Controller::Pcs8000Controller({slaveNo})')
         self.slaveNo = slaveNo
-
+        self.udpRunning = 0
         # List of stream values
-        self.streamList = [0]*16
+        self.streamValues = [0]*16
+        # List of streamValues to be streamed via udp
+        self.registeredStreams = list()
         self.sysState = State({"Busy":0,"Ready":1})
         self.seqState = State({"Setup":0,"Program":1})
 
+    def startUdp(self):
+        logging.info(f'Pcs8000Controller.startUDP() on slave {self.slaveNo}')
+        self.udpRunning = 1
+
+    # Function to set sys_state
     def setSysState(self,stateString):
+        logging.info(f'Pcs8000Controller::setSysState({stateString}) on slave {self.slaveNo}')
         self.sysState.setState(stateString)
-        print(f'Setting sys_state = "{stateString}" on slave {self.slaveNo}')
 
+    # Function to set seq_state
+    def setSeqState(self,stateString):
+        logging.info(f'Pcs8000Controller::setSeqState({stateString}) on slave {self.slaveNo}')
+        self.seqState.setState(stateString)
 
-class Pcs8000ControllerMaster(Pcs8000Controller):
-    def __init__(self):
-        Pcs8000Controller.__init__(self,0)
-        # Dict containing slaves, slave number is the key
-        self.slaves = dict()
-        # Set first element to be this object
-        self.slaves[0] = self
+    def registerStream(self,stream):
+        assert stream in range(1,16)
+        logging.info(f'Pcs8000Controller::registerStream({stream}) on slave {self.slaveNo}')
+        self.registeredStreams.append(stream)
 
-    def addSlave(self,slave):
-        self.slaves[int(slave.slaveNo)] = slave
-
-    def parseCommand(self,command):
-        commandRoot = ET.fromstring(command)
-        addressedSlave = int(commandRoot[0].text)
-
-        if commandRoot.tag == "udpxmit":
-            print("UDP related command")
-
-        if commandRoot.tag == "maincontrol":
-            target = commandRoot[1][0].tag
-            value = commandRoot[1][0].text.strip('"')
-            if target == "sys_state":
-                self.slaves[addressedSlave].setSysState(value)
-
-
-
-master = Pcs8000ControllerMaster()
-slave1 = Pcs8000Controller(1)
-slave2 = Pcs8000Controller(2)
-
-master.addSlave(slave1)
-master.addSlave(slave2)
-
-
-sysState = 0
-
-with open("../xmlCommands/setSysState.xml") as xmlCommand:
-    command = xmlCommand.read()
-
-master.parseCommand(command)
-
-print("End")
-
-
-
-
-
-
-
-#tree = ET.parse("../xmlCommands/setSysState.xml")
-#root = tree.getroot()
-#print(root[1][0])
+    def clearUDP(self):
+        logging.info(f'Pcs8000Controller.clearUDP() on slave {self.slaveNo}')
+        self.registeredStreams = list()
--- simulator/communicationSimulator.py
+++ simulator/communicationSimulator.py
@@ -2,7 +2,9 @@
 # streamDevice. Run this on a local machine and point the AsynIP port to localhost:8000.
 
 import socket
+import logging
 from Pcs8000Controller import Pcs8000Controller
+from Pcs8000ControllerMaster import Pcs8000ControllerMaster
 from threading import *
 
 
@@ -47,19 +49,67 @@ class tcpServer(Thread):
         while True:
             self.buffer+=self.serverSocket.recv(4096).decode()
             if(self.buffer.count("</") == self.buffer.count("<")/2):
-                print("Got xml:")
-                print(self.buffer)
+                response = masterController.parseCommand(self.buffer)
+                self.serverSocket.send(response.encode())
                 self.buffer=""
 
 
+class tcpClient(Thread):
+    def __init__(self, ipAddress, portNumber,masterController):
+        Thread.__init__(self)
+        self.masterController = masterController
+        self.ipAddress = ipAddress
+        self.portNumber = portNumber
+        self.buffer = ""
+        self.start()
+        self.data = {
+            "P":101,
+            "T":102,
+            "M":'R',
+            "L":0
+        }
 
+    def handShake(self):
+        # Send "HELLO\r\n"
+        self.serverSocket.send(f"HELLO\r\n".encode())
+
+        # Wait for response code and check it's correct
+        data = self.serverSocket.recv(4096)
+        assert data.rstrip()==b"DLS,1.00,1234","Driver sent %s" % data
+
+        # Send "OK\r\n"
+        self.serverSocket.send(f"OK\r\n".encode())
+
+    def run(self):
+
+        # Configure the socket
+        tcpCommandServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcpCommandServer.bind((self.ipAddress, self.portNumber))
+        tcpCommandServer.listen()
+        print(f"Listening on {self.ipAddress}:{self.portNumber}")
+
+        self.serverSocket, address = tcpCommandServer.accept()
+        print("Client connected")
+
+        self.handShake()
+        while True:
+            self.buffer+=self.serverSocket.recv(4096).decode()
+            if(self.buffer.count("</") == self.buffer.count("<")/2):
+                response = masterController.parseCommand(self.buffer)
+                self.serverSocket.send(response.encode())
+                self.buffer=""
+logging.basicConfig(level=logging.DEBUG)
 # Ports:
 tcpCommandPort = 51512
 udpStreamPort = 51513
 tcpEventPort = 51515
 
 # Create controller objects:
-masterController = Pcs8000Controller(0)
+masterController = Pcs8000ControllerMaster()
+slave1 = Pcs8000Controller(1)
+slave2 = Pcs8000Controller(2)
+masterController.addSlave(slave1)
+masterController.addSlave(slave2)
 
 # Create main TCP command server
 tcpServer("localhost",tcpCommandPort,masterController)
